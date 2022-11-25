5a13ac734f41fa6b9469d78bb4f23caf472c30e0
deafleopard76543 <mjdhn24@gmail.com>
2022-11-24 21:37:14 -0500

    added ghost and vampire squirrel functionalities

--- squirrel-eat-squirrel-main.py
+++ squirrel-eat-squirrel-main.py
@@ -25,7 +25,7 @@ GOLD = (212,175,55)
 
 #assignment of the variables that will be used later in the creation of game functions   MD
 CAMERASLACK = 90     # how far from the center the squirrel moves before moving the camera
-MOVERATE = 9         # how fast the player moves
+moverate = 9         # how fast the player moves
 BOUNCERATE = 6       # how fast the player bounces (large is slower)
 BOUNCEHEIGHT = 30    # how high the player bounces
 STARTSIZE = 25       # how big the player starts off
@@ -33,6 +33,7 @@ WINSIZE = 300        # how big the player needs to be to win
 INVULNTIME = 2       # how long the player is invulnerable after being hit in seconds
 GAMEOVERTIME = 4     # how long the "game over" text stays on the screen in seconds
 MAXHEALTH = 3        # how much health the player starts with
+FREEZETIME = 3       # amount of time player will be frozen after colliding with a ghost squirrel MD
 
 NUMGRASS = 80        # number of grass objects in the active area
 NUMSQUIRRELS = 30    # number of squirrels in the active area
@@ -146,7 +147,7 @@ def main():  #allows the runGame() function to be put into script at the end    
         # starts game once setup is complete SS
 
 def runGame():
-    global bestTime
+    global bestTime, moverate
     
     pygame.mixer.music.rewind() #rewind the music before each newgame AL
     pygame.mixer.music.play(loops=-1) #play music and keep looping it AL
@@ -157,6 +158,8 @@ def runGame():
     gameOverStartTime = 0     # time the player lost
     winMode = False           # if the player has won
     newBestMode = False       # if the player has beaten the previous best time AL
+    frozenmode = False        # if player avoids ghost squirrel MD
+    frozenmodestarttime = 0   
 
     gameStartTime = time.time() # record the start of a game relative to epoch AL
 
@@ -213,6 +216,11 @@ def runGame():
         if invulnerableMode and time.time() - invulnerableStartTime > INVULNTIME:
             invulnerableMode = False
 
+        # turns off the frozen mode     MD
+        if frozenmode and time.time() - frozenmodestarttime > FREEZETIME:
+            frozenmode = False
+            moverate = 9
+
         # move all the enemy squirrels
         # could reduce the bounce by decreasing BOUNCERATE to have levitating, ghost squrrels   MD
         for sObj in squirrelObjs:
@@ -262,7 +270,7 @@ def runGame():
             elif 80 < random_sqr_int <= 90:
                 squirrelObjs.append(makeNewSquirrel(camerax, cameray, L_GHOST_IMG, R_GHOST_IMG,'sqghost'))
             else:
-                squirrelObjs.append(makeNewSquirrel(camerax, cameray, L_VAMP_IMG, R_VAMP_IMG,'squampire'))
+                squirrelObjs.append(makeNewSquirrel(camerax, cameray, L_VAMP_IMG, R_VAMP_IMG,'squampire', faster = True))
 
 
         # Find the distance between game world coordinates of the centre of the camera
@@ -373,19 +381,19 @@ def runGame():
         if not gameOverMode:
             # actually move the player
             if moveLeft:
-                playerObj['x'] -= MOVERATE
+                playerObj['x'] -= moverate
                 if not pygame.mixer.get_busy(): # ensures sound effect is played to completion before it is played again, might cause issues with later sound effects AL
                     BOUNCESOUND.play()
             if moveRight:
-                playerObj['x'] += MOVERATE
+                playerObj['x'] += moverate
                 if not pygame.mixer.get_busy():
                     BOUNCESOUND.play()
             if moveUp:
-                playerObj['y'] -= MOVERATE
+                playerObj['y'] -= moverate
                 if not pygame.mixer.get_busy():
                     BOUNCESOUND.play()
             if moveDown:
-                playerObj['y'] += MOVERATE
+                playerObj['y'] += moverate
                 if not pygame.mixer.get_busy():
                     BOUNCESOUND.play()
 
@@ -400,6 +408,13 @@ def runGame():
                 sqObj = squirrelObjs[i]
                 if 'rect' in sqObj and playerObj['rect'].colliderect(sqObj['rect']):
                     # a player/squirrel collision has occurred using pygames rect module to check AL
+
+                    # if player runs into a ghost squirrel, they will be unable to move for a set amount of time  MD
+                    if sqObj['id'] == 'sqghost':
+                        frozenmode = True
+                        frozenmodestarttime = time.time()
+                        moverate = 0
+
                     if sqObj['width'] * sqObj['height'] <= playerObj['size']**2:
                         # player is larger and eats the squirrel
                         playerObj['size'] += int( (sqObj['width'] * sqObj['height'])**0.2 ) + 1
@@ -530,7 +545,7 @@ def getRandomOffCameraPos(camerax, cameray, objWidth, objHeight):
 
 # creates new enemy squirrels  of different sizes with placement, speed and direction determined randomly SS
 # Added new parameters for different enemy squirrels AL
-def makeNewSquirrel(camerax, cameray, leftSquirImg, rightSquirImg, sqrID):
+def makeNewSquirrel(camerax, cameray, leftSquirImg, rightSquirImg, sqrID, faster = False):
     """
     (int,int) -> dict
     Create a new squirrel "object", type specifies the type of squirrel AL
@@ -541,8 +556,13 @@ def makeNewSquirrel(camerax, cameray, leftSquirImg, rightSquirImg, sqrID):
     sq['width']  = (generalSize + random.randint(0, 10)) * multiplier
     sq['height'] = (generalSize + random.randint(0, 10)) * multiplier
     sq['x'], sq['y'] = getRandomOffCameraPos(camerax, cameray, sq['width'], sq['height'])
-    sq['movex'] = getRandomVelocity()
-    sq['movey'] = getRandomVelocity()
+    # allows optional adjustable speed arguement for different squirrels   MD
+    if faster == True:
+        sq['movex'] = getRandomVelocity() * 7
+        sq['movey'] = getRandomVelocity() * 6.9
+    else:
+        sq['movex'] = getRandomVelocity()
+        sq['movey'] = getRandomVelocity()
     sq['leftimg'] = leftSquirImg #store specific squirrels left and right images AL
     sq['rightimg'] = rightSquirImg
     if sq['movex'] < 0: # squirrel is facing left
